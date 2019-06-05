cf2482b82de4d7f39bfe956b045d2a7541cb4e29
Adiv Ohayon <adivohayon@gmail.com>
2019-06-05 12:35:38 +0300

    analytics pixel

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -5,23 +5,30 @@ const TRANSPARENT_GIF_BUFFER = Buffer.from(
 );
 
 module.exports = (app, {}) => {
-	app.get('/pixel.gif', async (req, res) => {
+	app.get('/pixel', async (req, res) => {
 		console.log('TRANSPARENT_GIF_BUFFER', TRANSPARENT_GIF_BUFFER.length);
 		const analytics = new Analytics('UA-134688384-1');
 
 		const { action, storeSlug, influencer } = req.query;
-
-		switch (action) {
-			case 'checkoutComplete':
-				await analytics.checkoutComplete(storeSlug, influencer);
-				break;
-			default:
-				await analytics.pixelLoaded();
-				break;
+		if (action) {
+			try {
+				switch (action) {
+					case 'checkoutComplete':
+						await analytics.checkoutComplete(storeSlug, influencer);
+						break;
+					default:
+						await analytics.pixelLoaded();
+						break;
+				}
+				res.writeHead(200, { 'Content-Type': 'image/gif' });
+				// res.end(TRANSPARENT_GIF_BUFFER, 'binary');
+				res.end(new Buffer(TRANSPARENT_GIF_BUFFER, 'binary'));
+			} catch (err) {
+				console.error(err);
+				res.sendStatus(500).send(err.toString());
+			}
 		}
 
-		res.writeHead(200, { 'Content-Type': 'image/gif' });
-		res.end(TRANSPARENT_GIF_BUFFER, 'binary');
 		// res.writeHead(200, {
 		// 	'Content-Type': 'image/gif',
 		// 	'Content-Length': TRANSPARENT_GIF_BUFFER.length,
--- services/analytics.service.js
+++ services/analytics.service.js
@@ -1,6 +1,7 @@
 const ua = require('universal-analytics');
 module.exports = class Analytics {
 	constructor(gaId) {
+		console.log('gaId', gaId);
 		this.visitor = ua(gaId);
 	}
 
@@ -17,19 +18,22 @@ module.exports = class Analytics {
 	}
 	checkoutComplete(storeSlug, influencer) {
 		return new Promise((resolve, reject) => {
-			this.visitor.event(
-				storeSlug + '__checkout',
-				'checkoutComplete',
-				'influencer',
-				influencer,
-				err => {
-					if (err) {
-						reject(err);
-					}
+			const eventParams = {
+				// hitType: 'event',
+				eventCategory: storeSlug + '__checkout',
+				eventAction: 'checkout__complete',
+				eventLabel: influencer,
+				// eventValue: 'aaaa',
+			};
 
-					resolve();
+			this.visitor.event(eventParams, function(err) {
+				if (err) {
+					console.log(err);
+					reject(err);
 				}
-			);
+				console.log('checkout complete');
+				resolve();
+			});
 		});
 	}
 };
